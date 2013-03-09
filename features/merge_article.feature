b738462a4b7e1a28bc690b6fe545fc85bfbdb4ec
Yury Tsarev <ytsarev@suse.com>
2013-03-09 22:11:07 +0100

    Do not expose merge functionality no non-admin users
    Signed-off-by: Yury Tsarev <ytsarev@suse.com>

--- features/merge_article.feature
+++ features/merge_article.feature
@@ -5,12 +5,18 @@ Feature: Merge Articles
 
   Background:
     Given the blog is set up
-    And I am logged into the admin panel
-    And I created "Merge1" article with "OLOLO1" content
-    And I created "Merge2" article with "LOLOLO2" content
 
   Scenario: Successfully merge articles
+    Given I am logged into the admin panel
+    And I created "Merge1" article with "OLOLO1" content
+    And I created "Merge2" article with "LOLOLO2" content
     Given I am on the "Merge1" article edit page
     When I fill in merge field with "Merge2" article id
     And I press "Merge"
     Then I should see the content of both "OLOLO1" and "LOLOLO2" 
+
+  Scenario:
+    Given I am logged into the user panel
+    And I created "User1" article with "TROLOLO1" content
+    Given I am on the "User1" article edit page
+    Then I should not see "Merge"
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -41,12 +41,31 @@ Given /^the blog is set up$/ do
                 :profile_id => 1,
                 :name => 'admin',
                 :state => 'active'})
+
+  User.create!({:login => 'user',
+                :password => '123123123',
+                :email => 'amie@snow.com',
+                :profile_id => 2,
+                :name => 'nonadmin',
+                :state => 'active'})
 end
 
 And /^I am logged into the admin panel$/ do
   visit '/accounts/login'
   fill_in 'user_login', :with => 'admin'
   fill_in 'user_password', :with => 'aaaaaaaa'
+  click_button 'Login'
+  if page.respond_to? :should
+    page.should have_content('Login successful')
+  else
+    assert page.has_content?('Login successful')
+  end
+end
+
+And /^I am logged into the user panel$/ do
+  visit '/accounts/login'
+  fill_in 'user_login', :with => 'user'
+  fill_in 'user_password', :with => '123123123'
   click_button 'Login'
   if page.respond_to? :should
     page.should have_content('Login successful')
